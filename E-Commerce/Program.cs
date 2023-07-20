d14cd455aa66db1b2f89cb2e109f1f9ece521290
MohamedMokhtar6 <mohamed01017715716@gmail.com>
2023-07-20 23:33:08 +0300

    س

--- E-Commerce/Program.cs
+++ E-Commerce/Program.cs
@@ -59,11 +59,11 @@ builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{}
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 app.UseCors(c => c.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
